8e316b0239d1889a086092a8d534aa3de5bef420
FMondragon7 <fernandomondragon07@gmail.com>
2024-03-08 21:30:27 -0800

    feat: add images to targaryen and stark

--- src/components/appSection.jsx
+++ src/components/appSection.jsx
@@ -1,6 +1,7 @@
 
 import ColumnSection from "./columnSection.jsx";
 
+// house emblems
 import arryn from "@/assets/arryn.png";
 import greyjoy from "@/assets/greyjoy.png";
 import lannister from "@/assets/lannister.png";
@@ -9,8 +10,31 @@ import stark from "@/assets/stark.png";
 import targaryen from "@/assets/targaryen.png";
 import tully from "@/assets/tully.png";
 import tyrell from "@/assets/tyrell.png";
+import free_folk from "@/assets/free_folk.png";
 
+//lannister members
 import cersei from "@/assets/cersei.png";
+import jaime from "@/assets/jaime.png";
+import tywin from "@/assets/tywin.png";
+import tyrion from "@/assets/tyrion.png";
+
+//stark members
+import eddard from "@/assets/eddard.png";
+import catelyn from "@/assets/catelyn.png";
+import robb from "@/assets/robb.png";
+import arya from "@/assets/arya.png";
+import sansa from "@/assets/sansa.png";
+import brandon from "@/assets/brandon.png";
+import talisa from "@/assets/talisa.png";
+// import rickon from "@/assets/rickon.png";
+
+//targaryen members
+import aegonVI from "@/assets/aegon_vi.png";
+import daenerys from "@/assets/daenerys.png";
+import viserys from "@/assets/viserys.png";
+import rhaenyra from "@/assets/rhaenyra.png";
+import daemon from "@/assets/daemon.png";
+
 
 import { useEffect, useState } from "react";
 
@@ -21,18 +45,28 @@ const HOUSES = [
     members: [
       {
         name: "Daenerys Targaryen",
-        image: "daenerys-targaryen.jpg",
+        image: daenerys,
         description: "Mother of Dragons",
       },
       {
+        name: "Aegon VI Targaryen",
+        image: aegonVI,
+        description: "The True King",
+      },
+      {
         name: "Viserys Targaryen",
-        image: "viserys-targaryen.jpg",
+        image: viserys,
         description: "The Beggar King",
       },
       {
-        name: "Aegon Targaryen",
-        image: "aegon-targaryen.jpg",
-        description: "The Conqueror",
+        name: "Rhaenyra Targaryen",
+        image: rhaenyra,
+        description: "The princess of dragonstone",
+      },
+      {
+        name: "Daemon  Targaryen",
+        image: daemon,
+        description: "The prince of the city",
       },
     ],
   },
@@ -41,19 +75,39 @@ const HOUSES = [
     image: stark,
     members: [
       {
+        name: "Arya Stark",
+        image: arya,
+        description: "No one",
+      },
+      {
+        name: "Brandon Stark",
+        image: brandon,
+        description: "Lord of Winterfell",
+      },
+      {
         name: "Eddard Stark",
-        image: "eddard-stark.jpg",
+        image: eddard,
         description: "Lord of Winterfell",
       },
       {
         name: "Catelyn Stark",
-        image: "catelyn-stark.jpg",
+        image: catelyn,
         description: "Lady of Winterfell",
       },
       {
         name: "Robb Stark",
-        image: "robb-stark.jpg",
+        image: robb,
         description: "King in the North",
+      },
+      {
+        name: "sansa Stark",
+        image: sansa,
+        description: "Lady of Winterfell",
+      },
+      {
+        name: "Talisa Stark",
+        image: talisa,
+        description: "Queen Consort",
       },
     ],
   },
@@ -62,19 +116,24 @@ const HOUSES = [
     image: lannister,
     members: [
       {
-        name: "Tywin Lannister",
-        image: "tywin-lannister.jpg",
-        description: "Lord of Casterly Rock",
+        name: "Cersei Lannister",
+        image: cersei,
+        description: "Lady of Casterly Rock",
       },
       {
         name: "Jaime Lannister",
-        image: "jaime-lannister.jpg",
-        description: "Kingslayer",
+        image: jaime,
+        description: "Lord Commander of the Kingsguard",
       },
       {
-        name: "Cersei Lannister",
-        image: cersei,
-        description: "Queen of the Seven Kingdoms",
+        name: "Tyrion Lannister",
+        image: tyrion,
+        description: "Hand of the Queen",
+      },
+      {
+        name: "Tywin Lannister",
+        image: tywin,
+        description: "Lord paramount of the Westerlands",
       },
     ],
   },
@@ -183,6 +242,22 @@ const HOUSES = [
       },
     ],
   },
+  {
+    name: "Free Folk",
+    image: free_folk,
+    members: [
+      {
+        name: "Tormund Giantsbane",
+        image: "Tormund-Giantsbane.jpg",
+        description: "Free Folk Warrior",
+      },
+      {
+        name: "Ygritte",
+        image: "Ygritte.jpg",
+        description: "Spear Wife",
+      },
+    ],
+  },
 ];
 
 export default function AppSection () {
@@ -197,8 +272,12 @@ export default function AppSection () {
   }, [selectedHouse])
   
   return (
-    <section className="flex flex-row gap-24 h-full animate-fade-in">
-      <ColumnSection title="Families" list={HOUSES} selectedItem={selectedHouse} setSelectedItem={setSelectedHouse} />
+    <section className="flex flex-row gap-24 h-full animate-fade-in" style={{ animationDelay: "1s" }}>
+      <ColumnSection 
+        title="Families" 
+        list={HOUSES} 
+        selectedItem={selectedHouse} 
+        setSelectedItem={setSelectedHouse} />
       <ColumnSection 
         customClass={`${!selectedHouse ? "animate-fade-out" : "animate-fade-in"}`}
         title="Members" 
