39aecc60a5ddf8778f097f71346e6d6a84a46bd0
FMondragon7 <fernandomondragon07@gmail.com>
2024-03-08 17:36:23 -0800

    add: new members sections

--- src/components/appSection.jsx
+++ src/components/appSection.jsx
@@ -10,49 +10,200 @@ import targaryen from "@/assets/targaryen.svg";
 import tully from "@/assets/tully.svg";
 import tyrell from "@/assets/tyrell.svg";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const HOUSES = [
   {
     name: "House Targaryen",
     image: targaryen,
+    members: [
+      {
+        name: "Daenerys Targaryen",
+        image: "daenerys-targaryen.jpg",
+        description: "Mother of Dragons",
+      },
+      {
+        name: "Viserys Targaryen",
+        image: "viserys-targaryen.jpg",
+        description: "The Beggar King",
+      },
+      {
+        name: "Aegon Targaryen",
+        image: "aegon-targaryen.jpg",
+        description: "The Conqueror",
+      },
+    ],
   },
   {
     name: "House Stark",
     image: stark,
+    members: [
+      {
+        name: "Eddard Stark",
+        image: "eddard-stark.jpg",
+        description: "Lord of Winterfell",
+      },
+      {
+        name: "Catelyn Stark",
+        image: "catelyn-stark.jpg",
+        description: "Lady of Winterfell",
+      },
+      {
+        name: "Robb Stark",
+        image: "robb-stark.jpg",
+        description: "King in the North",
+      },
+    ],
   },
   {
     name: "House Lannister",
     image: lannister,
+    members: [
+      {
+        name: "Tywin Lannister",
+        image: "tywin-lannister.jpg",
+        description: "Lord of Casterly Rock",
+      },
+      {
+        name: "Jaime Lannister",
+        image: "jaime-lannister.jpg",
+        description: "Kingslayer",
+      },
+      {
+        name: "Cersei Lannister",
+        image: "cersei-lannister.jpg",
+        description: "Queen of the Seven Kingdoms",
+      },
+    ],
   },
   {
     name: "House Tyrell",
     image: tyrell,
+    members: [
+      {
+        name: "Mace Tyrell",
+        image: "mace-tyrell.jpg",
+        description: "Lord of Highgarden",
+      },
+      {
+        name: "Olenna Tyrell",
+        image: "olenna-tyrell.jpg",
+        description: "Queen of Thorns",
+      },
+      {
+        name: "Loras Tyrell",
+        image: "loras-tyrell.jpg",
+        description: "Knight of Flowers",
+      },
+    ],
   },
   {
     name: "House Arryn",
     image: arryn,
+    members: [
+      {
+        name: "Jon Arryn",
+        image: "jon-arryn.jpg",
+        description: "Lord of the Eyrie",
+      },
+      {
+        name: "Lysa Arryn",
+        image: "lysa-arryn.jpg",
+        description: "Lady of the Eyrie",
+      },
+      {
+        name: "Robert Arryn",
+        image: "robert-arryn.jpg",
+        description: "Lord of the Eyrie",
+      },
+    ],
   },
   {
     name: "House Greyjoy",
     image: greyjoy,
+    members: [
+      {
+        name: "Balon Greyjoy",
+        image: "balon-greyjoy.jpg",
+        description: "Lord Reaper of Pyke",
+      },
+      {
+        name: "Yara Greyjoy",
+        image: "yara-greyjoy.jpg",
+        description: "Iron Queen",
+      },
+      {
+        name: "Theon Greyjoy",
+        image: "theon-greyjoy.jpg",
+        description: "Reek",
+      },
+    ],
   },
   {
     name: "House Martell",
     image: martell,
+    members: [
+      {
+        name: "Doran Martell",
+        image: "doran-martell.jpg",
+        description: "Prince of Dorne",
+      },
+      {
+        name: "Oberyn Martell",
+        image: "oberyn-martell.jpg",
+        description: "Red Viper",
+      },
+      {
+        name: "Elia Martell",
+        image: "elia-martell.jpg",
+        description: "Princess of Dorne",
+      },
+    ],
   },
   {
     name: "House Tully",
     image: tully,
+    members: [
+      {
+        name: "Hoster Tully",
+        image: "hoster-tully.jpg",
+        description: "Lord of Riverrun",
+      },
+      {
+        name: "Catelyn Tully",
+        image: "catelyn-tully.jpg",
+        description: "Lady of Riverrun",
+      },
+      {
+        name: "Edmure Tully",
+        image: "edmure-tully.jpg",
+        description: "Lord of Riverrun",
+      },
+    ],
   },
 ];
 
 export default function AppSection () {
   const [selectedHouse, setSelectedHouse] = useState(null);
+  const [selectedMember, setSelectedMember] = useState(null);
+  const [prevMembers, setPrevMembers] = useState(null)
+
+  useEffect(() => {
+    if (selectedHouse) {
+      setPrevMembers(HOUSES.find(house => house.name === selectedHouse)?.members || [])
+    }
+  }, [selectedHouse])
   
   return (
-    <section className="flex flex-row gap-6 h-full animate-fade-in">
+    <section className="flex flex-row gap-24 h-full animate-fade-in">
       <ColumnSection title="Families" list={HOUSES} selectedItem={selectedHouse} setSelectedItem={setSelectedHouse} />
+      <ColumnSection 
+        customClass={`${!selectedHouse ? "animate-fade-out" : "animate-fade-in"}`}
+        title="Members" 
+        list={HOUSES.find(house => house.name === selectedHouse)?.members || prevMembers || []} 
+        selectedItem={selectedMember} 
+        setSelectedItem={setSelectedMember}
+      />
     </section>
   )
 }
--- src/components/columnSection.jsx
+++ src/components/columnSection.jsx
@@ -1,6 +1,15 @@
 export default function ColumnSection(props) {
+
+  const handleSelect = (e) => {
+    if(props.selectedItem === e.currentTarget.id) {
+      props.setSelectedItem(null)
+      return
+    }
+    props.setSelectedItem(e.currentTarget.id)
+  }
+  
   return (
-    <section className="min-w-[350px] flex flex-col gap-6">
+    <section className={`min-w-[350px] flex flex-col gap-6 ${props.customClass}`}>
       <span className="text-semibold text-grey tracking-[4px]">{props.title}</span>
         <div
           className="bg-primary p-8 flex flex-col gap-4 max-h-full overflow-y-auto"
@@ -12,21 +21,21 @@ export default function ColumnSection(props) {
                   key={item.name}
                   className={`w-full h-20 bg-secondary px-6 py-4 flex flex-row text-center items-center gap-4 cursor-pointer
                   ${props.selectedItem === item.name ? "bg-white" : ""}
-                  hover:border hover:border-solid hover:border-amber-950 hover: transition-all duration-50 ease-in-out`}
+                  hover:border hover:border-solid hover:border-amber-950 hover:transition-all duration-50 ease-in-out`}
                   id={item.name}
-                  onClick={(e) => {
-                    if(props.selectedItem === e.currentTarget.id) {
-                      props.setSelectedItem(null)
-                      return
-                    }
-                    props.setSelectedItem(e.currentTarget.id)
-                  }}
+                  onClick={(e) => handleSelect(e)}
                 >
                   <img className="w-12 h-12 pointer-events-none" src={item.image.src} alt="item image" 
                     style={{ filter: `${props.selectedItem === item.name ? "brightness(1) invert(0)" : "brightness(0) invert(1)"}` }} />
-                    <div>
-                      <span className={`pointer-events-none ${props.selectedItem === item.name ? "text-secondary" : "text-white"}`}>{item.name}</span>
-                      {item.description && <span className={`pointer-events-none ${props.selectedItem === item.name ? "text-secondary" : "text-white"}`}>{item.description}</span>}
+                    <div className={item.description ? "flex flex-col gap-2  items-start" : ""}>
+                      <span className={`pointer-events-none ${props.selectedItem === item.name ? "text-secondary" : "text-white"}`}>
+                        {item.name}
+                      </span>
+                      {item.description && 
+                        <span className="text-xs text-semibold font-light text-description pointer-events-none ">
+                          {item.description}
+                        </span>
+                      }
                     </div>
                 </div>
               );
